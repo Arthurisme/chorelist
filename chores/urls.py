8459b81da97bc92313dd24650cda814cf87f94ee
Arthurisme <is@live.ca>
2015-10-18 13:00:13 -0400

    Begin exception page

--- chores/urls.py
+++ chores/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
 from django.conf.urls import include, url
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^chores/', include('chores.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',  views.index, name='index'),
+    url(r'^(?P<chorelist_id>[0-9]+)/$',  views.detail, name='detail'),
+    url(r'^(?P<chorelist_id>[0-9]+)/chores/$',  views.chores, name='chores'),
+    url(r'^(?P<chorelist_id>[0-9]+)/chores/(?P<chore_id>[0-9]+)/$',  views.choredetail, name='choredetail')
+
 ]
--- chores/views.py
+++ chores/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from .models import  ChoreList
+from django.template import RequestContext,loader
+
+def index(request):
+    # 04-01 simple page
+    # return HttpResponse("You are at the ChoreList index.")
+
+    #read all chorelist data to view and show:
+    lists = ChoreList.objects.all()
+    # template=loader.get_template('chores/index.html')
+    # context=RequestContext(request,{
+    #     'chorelists':lists
+    # })
+    # output = ', '.join([cl.name for cl in lists])
+    # return HttpResponse(template.render(context))
+    context= {  'chorelists':lists }
+    return render(request,'chores/index.html',context)
+
+def detail(request, chorelist_id):
+    # return HttpResponse("You are looking at ChoreList #%s ." % chorelist_id)
+    lists = ChoreList.objects.get(pk=chorelist_id)
+    # template=loader.get_template('chores/index.html')
+    # context=RequestContext(request,{
+    #     'chorelists':lists
+    # })
+    # output = ', '.join([cl.name for cl in lists])
+    # return HttpResponse(template.render(context))
+    context= {  'chorelist':lists }
+    return render(request,'chores/detail.html',{'chorelist': lists})
+
+def chores(request, chorelist_id):
+    return HttpResponse("You are looking at Chores from ChoreList #%s ." % chorelist_id)
+
+def choredetail(request, chorelist_id, chore_id):
+    return HttpResponse("You are looking at Chores #%s from ChoreList #%s ." % (chore_id,chorelist_id))
+
